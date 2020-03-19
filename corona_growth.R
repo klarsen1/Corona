a06931e35c67812ac20af12588bbd9ff20a3cef0
Kim Larsen <kim.larsen@RTD.local>
2020-03-19 06:50:06 -0700

    recalculate new casses for first row

--- corona_growth.R
+++ corona_growth.R
@@ -3,14 +3,12 @@ library(readr)
 
 cutoff <- Sys.Date()
 
-d <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") 
-
 read_corona <- function(file, variable){
   f <- file
   d <- read_csv(f) %>%
      pivot_longer(cols = matches("1|2|3|4|5|6|7|8|9"), names_to = "date", values_to = variable) %>%
      rename(country_region=`Country/Region`, lat=Lat, lon=Long, province_state=`Province/State`) %>%
-     mutate(date=as.Date(gsub("X", "", date), "%m/%d/%y"), 
+     mutate(date=as.Date(date, "%m/%d/%y"), 
             province_state=ifelse(province_state=="", "NA", province_state))
   return(d)
 }
@@ -26,24 +24,29 @@ corona <-
   replace(is.na(.), 0) %>%
   arrange(country_region, province_state) %>%
   group_by(country_region, province_state) %>%
-  mutate(new_confirmed_cases=confirmed_cases-lag(confirmed_cases), 
-         new_deaths=deaths-lag(deaths),
-         new_recoveries=recovered-lag(recovered)) %>%
+  mutate(row=row_number(), 
+         new_confirmed_cases=ifelse(row>1, confirmed_cases-lag(confirmed_cases), confirmed_cases),  
+         new_deaths=ifelse(row>1, deaths-lag(deaths), deaths),
+         new_recoveries=ifelse(row>1, recovered-lag(recovered), recovered)) %>%
   ungroup()
 
-ts <- 
-  group_by(corona,  country_region, date) %>% 
+us <- 
+  corona %>%
+  group_by(country_region, date) %>% 
   summarise_at(vars(new_confirmed_cases, new_deaths, new_recoveries), funs(sum)) %>% 
   mutate(new_case_growth_rate=new_confirmed_cases/lag(new_confirmed_cases)-1) %>%
   filter(country_region=="US" & date >= as.Date("2020-03-01"))
 
-ggplot(ts, aes(x=date, y=new_confirmed_cases)) + geom_line() + scale_x_date(date_breaks = "1 day") + 
+ggplot(us, aes(x=date, y=new_confirmed_cases)) + 
+  geom_line() + 
+  scale_x_date(date_breaks = "1 day") + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   scale_y_continuous(labels = scales::comma) +
   xlab("Date") + ylab("New Confirmed Cases")
 
-ggplot(ts, aes(x=date, y=new_case_growth_rate)) + geom_line() + scale_x_date(date_breaks = "1 day") + 
+ggplot(us, aes(x=date, y=new_case_growth_rate)) + 
+  geom_line() + 
+  scale_x_date(date_breaks = "1 day") + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   scale_y_continuous(labels = scales::percent) +
   xlab("Date") + ylab("New Confirmed Case Growth Rate")
-
