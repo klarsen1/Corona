c8a3486698f15e8b09e456548988f6b52ce96a85
Kim Larsen <kim.larsen@RTD.local>
2020-03-22 19:38:56 -0700

    raw cohort plots

--- cohort_plots.R
+++ cohort_plots.R
@@ -11,21 +11,25 @@ d <- readRDS("corona_country_cohorts.rda") %>%
   filter(cohort_index_first_death>0)
 
 m1 <- ceiling(max(d$cumulative_deaths_scaled)/1000)*1000
-b1 <- seq(from=0, to=m1, by=2000)
+b1 <- seq(from=0, to=m1, by=1000)
 m2 <- ceiling(max(d$cumulative_cases_scaled)/10000)*10000
-b2 <- seq(from=0, to=m2, by=20000)
+b2 <- seq(from=0, to=m2, by=5000)
 
-ggplot(data=d, aes(x=cohort_index_first_death,  y=cumulative_deaths_scaled, colour=country_region)) +
+ggplot(data=d, aes(x=cohort_index_first_death,  y=cumulative_deaths, colour=country_region)) +
   geom_point(aes(shape=country_region), size=2) + geom_line(size=1) + 
   scale_y_continuous(labels = scales::comma, breaks=b1) +
-  ylab("Total Deaths (US population equivalent)") + 
+  ylab("Total Deaths") + 
   xlab("Days Since First Death") + 
   theme(legend.title = element_blank())
 
 
-ggplot(data=d, aes(x=cohort_index_first_case,  y=cumulative_cases_scaled, colour=country_region)) +
+c <- readRDS("corona_country_cohorts.rda") %>%
+  filter(!(country_region %in% c("China", "Singapore", "Iran", "United Kingdom", "Japan"))) %>%
+  filter(cohort_index_first_case>0)
+
+ggplot(data=c, aes(x=cohort_index_first_case,  y=cumulative_cases, colour=country_region)) +
   geom_point(aes(shape=country_region), size=2) + geom_line(size=1) +
   scale_y_continuous(labels = scales::comma,  breaks=b2) +
-  ylab("Total Cases (US population equivalent)") + 
+  ylab("Total Cases") + 
   xlab("Days Since First Case") + 
   theme(legend.title = element_blank())
